50bdb74ec1401528656f215666e369b483a58c82
Ahmad Sattar <thehabbos007@gmail.com>
2018-10-29 12:08:51 +0100

    Test localization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main {
     public static void main(String[] args) {
+        Locale currentLocale = new Locale("da", "DK");
+        ResourceBundle messages = ResourceBundle.getBundle("Translation", currentLocale);
+
+        String msg1 = messages.getString("greetings");
+        System.out.println(msg1);
         System.out.println("mems");
     }
 }
