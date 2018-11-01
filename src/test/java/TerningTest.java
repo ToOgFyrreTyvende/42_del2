32538cd1a386b152dbe0ea184ca6e9bad21895a1
Ahmad Sattar <thehabbos007@gmail.com>
2018-11-01 20:46:25 +0100

    tests and changes to some of the classes

--- src/test/java/TerningTest.java
+++ src/test/java/TerningTest.java
@@ -1,5 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TerningTest {
+    Terning terning = new Terning();
 
+    @Test
+    void getResultat() {
+        Random rng = new Random();
+        int[] dist = new int[12];
+        int[] slag = new int[12];
+
+        // Vi slår 50000 gange
+        for (int i = 1; i <= 5; i++) {
+            int tempSlag = terning.getResultat()[2];
+            slag[tempSlag - 1]++;
+
+            double gaus = (rng.nextGaussian() * 2) + 7;
+            if (gaus < 13 && gaus > 1) {
+                int tempGaus = (int) gaus;
+                dist[tempGaus - 1]++;
+            }
+        }
+
+        for (int i = 0; i < slag.length; i++) {
+            int goal = dist[i];
+            assertTrue(slag[0] >= goal-2000 && slag[0] <= goal+2000);
+        }
+    }
 }
--- src/test/java/kontoTest.java
+++ src/test/java/kontoTest.java
@@ -1,4 +1,22 @@
-package java;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class kontoTest {
+    @Test
+    void kontroTest(){
+        Konto konto = new Konto();
+        assertTrue(konto.getPenge() == 1000);
+
+        konto.indsaetpenge(20);
+        assertTrue(konto.getPenge() == 1020);
+
+        konto.indsaetpenge(1);
+        assertTrue(konto.getPenge() == 1021);
+
+        konto.indsaetpenge(-21);
+        assertTrue(konto.getPenge() == 1000);
+
+    }
+
 }
