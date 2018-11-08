e0225a7ed48f7281165874f2b25dabb4caef672c
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2018-11-08 19:05:49 +0100

    Revert "Updated FeltlisteTest"
    This reverts commit 4837501ab94f26ec6bffbec35722c961405ffea6.

--- src/test/java/FeltlisteTest.java
+++ src/test/java/FeltlisteTest.java
@@ -12,25 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FeltlisteTest {
 
     @Test
-    void getFeltPengeTest() {
-        // Vi tester om getFeltPenge får den korrekte værdi på 2 forskellige felter
-        int testPenge = 250;
-        int testPenge2 = -50;
-        assertEquals(testPenge, Feltliste.getFeltPenge(0));
-        assertEquals(testPenge2, Feltliste.getFeltPenge(9));
-    }
-
-    @Test
-    void getFeltTekstTest() {
-        // Vi tjekker at getFeltTekst finder den korrekte string i et arbitrært felt
-        String testTekst = "Du snubler og taber 80 guld ned i Hullet.";
-        assertEquals(testTekst, Feltliste.getFeltTekst(9));
-    }
-
-    @Test
-    void getEkstraTurTest() {
-        // Vi tester om getEkstraTur kun returnerer 'true' ved felt 10 (8 i arrayet)
-        assertEquals(true, Feltliste.getEkstraTur(8));
-        assertEquals(false, Feltliste.getEkstraTur(2));
+    void WelcomePlayerTest() {
+        String testTekst = "Velkommen til IOOuteractive spillet!\nIndtast venligst 2 spiller navne for paabegynde et spil med 2 spillere.";
+        assertEquals(testTekst, Feltliste.feltTekst.getString("WelcomePlayer"));
     }
 }
