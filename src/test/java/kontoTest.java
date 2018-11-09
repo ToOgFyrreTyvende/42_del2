e4f0792a034edc5eb5a2f920bc7dbdd7f1f6560a
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2018-11-09 09:19:56 +0100

    Update src/test/java/kontoTest.java
    Added test for when a player loses more money than they have - should return as 0.
    
    Renamed kontoTest to indsaetPengeTest
    (Since kontoTest doesn't test EVERYTHING so... better ocd name xd)
    
    Co-Authored-By: Ahmad Sattar <thehabbos007@users.noreply.github.com>
    Co-Authored-By: isling98 <isling98@users.noreply.github.com>
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>
    Co-Authored-By: lysberg <lysberg@users.noreply.github.com>
    Co-Authored-By: flaremew <flaremew@users.noreply.github.com>

--- src/test/java/kontoTest.java
+++ src/test/java/kontoTest.java
@@ -22,7 +22,7 @@ public class kontoTest {
     }
 
     @Test
-    void kontroTest(){
+    void indsaetPengeTest(){
         Konto konto = new Konto();
         assertTrue(konto.getPenge() == 1000);
 
@@ -35,6 +35,9 @@ public class kontoTest {
         konto.indsaetpenge(-21);
         assertTrue(konto.getPenge() == 1000);
 
+        konto.setPenge(10);
+        konto.indsaetpenge(-42);
+        assertTrue(konto.getPenge() == 0);
     }
 
     @Test
